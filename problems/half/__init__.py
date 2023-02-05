aa41964d8700f723d1d0054620749a7dc95a366a
Marcus T <mtwyford@users.noreply.github.com>
2023-02-05 16:48:38 -0500

    Update __init__.py

--- problems/half/__init__.py
+++ problems/half/__init__.py
@@ -15,8 +15,3 @@ def compiles():
 def emma():
     """responds to name Emma"""
     check50.run("./hello").stdin("Emma").stdout("Emma").exit()
-
-@check50.check(compiles)
-def rodrigo():
-    """responds to name Rodrigo"""
-    check50.run("./hello").stdin("Rodrigo").stdout("Rodrigo").exit()
